1ebe9602ead119f208507923cffa1a5fd6a65881
slarsar <slarsar@yandex.com>
2019-10-29 20:20:07 +0800

    add infinite loop with do

--- break-loop/break.lisp
+++ break-loop/break.lisp
@@ -1,8 +1,16 @@
-(defun find-first-odd (list-of-nums)
-  (dolist (e list-of-nums)
-    (format t "~&Tesing : get ~S" e)
-    (when (oddp e)
-      (format t "~&get odd :~S" e)
-      (return e))))
+					;(defun find-first-odd (list-of-nums)
+					;  (dolist (e list-of-nums)
+					;    (format t "~&Tesing : get ~S" e)
+					;    (when (oddp e)
+					;      (format t "~&get odd :~S" e)
+					;      (return e))))
+					;
+					;(find-first-odd '(2 3 4 5 7 8))
 
-(find-first-odd '(2 2 4 5 7 8))
+(defun ffo-with-do (list-of-numbers)
+  (do ((x list-of-numbers
+	  (rest x)))
+      ((null x) nil)
+    (if (oddp (first x))
+	(return (first x)))))
+(trace ffo-with-do '(1 2 3 4))
